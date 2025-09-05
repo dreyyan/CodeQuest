23992f5a942526c9fd7c2cc3c42a7d7575d1b353
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-09-05 13:13:33 +0800

    Initial Commit

--- Python/level-2/problem_05_count_to_10.py
+++
@@ -1,6 +0,0 @@
-""" ============================
-LEVEL 2 : [ Problem #5 - Count to 10 ]
-1. Use a loop to print numbers 1 to 10.
-============================ """
-# your code goes here
-
--- Python/level-6/problem_02_bank_management_system.py
+++
@@ -1,8 +0,0 @@
-""" ============================
-LEVEL 6 : [ Problem #2 - Bank Management System ]
-Define a BankAccount class with attributes for account holder's name and balance.
-Include methods to deposit, withdraw (ensure balance doesn't go negative), and get the current balance.
-============================ """
-class BankAccount:
-    # your code goes here
-    pass
